1fc49902d3b0b6a185eea20d923f3ccd568a0cc8
ju4nqleyton <ju4nqleyton@gmail.com>
2025-06-24 00:08:07 -0500

    componente Button funcionando

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
+import Button from "../components/Button";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Banner />
-    </div>
+      <Button />
+    </>
   );
 }
--- components/Banner.js
+++ components/Banner.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Banner() {
   return (
-    <div className="flex flex-col gap-8 items-center min-h-screen p-8 pb-20">
+    <div className="flex flex-col gap-8 items-center p-8">
       <h1 className="text-2xl font-bold">ejercicio-practico-git</h1>
       <Image
         src="/git-github.webp"
